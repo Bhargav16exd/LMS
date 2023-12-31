e72427502952580c242bcdeefa8feed264e55732
Bhargav Mule <bhargavmule16@gmail.com>
2023-12-31 17:36:04 +0530

    basic server setup

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express, { urlencoded } from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+// Basic Setup for server 
+
+app.use(urlencoded({extended:true}))
+app.use(cors({
+    origin:process.env.ORIGIN
+}))
+app.use(cookieParser())
+app.use(express.static("public"))
+app.use(express.json({
+    limit:"16kb"
+}))
+
+// Routes
+
+app.all('/',(req,res)=>{
+     res.json({
+        message:"HOMEPAGE"
+     })
+})
+
+
+export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import app from "./app.js"
+import connectionToDb from "./db/db.js"
+
+const PORT = process.env.PORT
+
+// dot env config
+
+dotenv.config(
+    {path: './.env'}
+)
+
+connectionToDb()
+
+.then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+      })
+})
+.catch((err)=>{
+    console.log("Error While Connecting to Database : ", err);
+})

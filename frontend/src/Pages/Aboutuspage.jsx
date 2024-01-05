a8821dda40749c7161101addd3de3461f594e13b
Bhargav Mule <bhargavmule16@gmail.com>
2024-01-05 12:13:57 +0530

    made core more readable

--- frontend/src/Pages/Aboutuspage.jsx
+++ frontend/src/Pages/Aboutuspage.jsx
@@ -1,5 +1,6 @@
 import Homelayout from "../Layout/Homelayout";
 
+
 // image imports
 import aboutMainImage from "../assets/aboutMainImage.png"
 import apj from "../assets/apj.png"
@@ -7,9 +8,53 @@ import bill from "../assets/billGates.png"
 import sj from "../assets/steveJobs.png"
 import einstein from "../assets/einstein.png"
 import nelson from "../assets/nelsonMandela.png"
+import Cor from "../components/Corosuol";
 
 function Aboutus(){
     
+    const data = [
+        {    
+            currSlide:"slide1",
+            name:"Steve Jobs",
+            quote:"Embrace the challenge, for within adversity lies the seed of your greatest growth",
+            image:sj,
+            prevSlide:"#slide4",
+            nextSlide:"#slide2"
+
+        },
+        {    
+            currSlide:"slide2",
+            name:"Bill Gates",
+            quote:"Every step forward, no matter how small, is a victory in the journey of self-improvement.",
+            image:bill,
+            prevSlide:"#slide1",
+            nextSlide:"#slide3"
+        },
+        {   
+            currSlide:"slide3",
+            name:"Nelson Mandela",
+            quote:"Believe in your potential; the power to transform your dreams into reality resides within you.",
+            image:nelson,
+            prevSlide:"#slide2",
+            nextSlide:"#slide4"
+        },
+        {    
+            currSlide:"slide4",
+            name:"APJ Abdul Kalam",
+            quote:"In the dance of life, let passion be your music and determination your graceful steps.",
+            image:apj,
+            prevSlide:"#slide3",
+            nextSlide:"#slide5"
+        },
+        {   
+            currSlide:"slide5",
+            name:"Albert Einstein",
+            quote:"Your potential is like a seed; nurture it with belief, water it with effort",
+            image:einstein,
+            prevSlide:"#slide4",
+            nextSlide:"#slide1"
+        },
+    ]
 
     return(
         <Homelayout>
@@ -30,45 +75,16 @@ function Aboutus(){
                     </div>
                 </div>
 
-                <div className=" my-5 flex justify-center py-4"> 
+                <div className="border border-gray-400 mr-10"></div>
+
+                <div className=" my-5 flex justify-center py-10"> 
 
                  <div className="innerBox w-1/2 flex flex-col justify-center align-middle">
                      
                    <div className="carousel w-full">
-                    <div id="slide1" className="carousel-item relative w-full flex justify-center align-middle">
-                        <div className="flex flex-col justify-center items-center ">
-                        <img src={sj} className="w-40 h-40 rounded-full " />
-                        <h1 className="text-center py-5 text-2xl font-semibold">Steve Jobs</h1>
-                        <p>
-                        "Embrace the challenge, for within adversity lies the seed of your greatest growth"
-                        </p>
-                        </div>
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a> 
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                        </div>
-                    </div> 
-                    <div id="slide2" className="carousel-item relative w-full flex justify-center align-middle">
-                        <img src={einstein} className="h-40 rounded-full" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a> 
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                        </div>
-                    </div> 
-                    <div id="slide3" className="carousel-item relative w-full flex justify-center align-middle">
-                        <img src={nelson} className="h-40 rounded-full" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a> 
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                        </div>
-                    </div> 
-                    <div id="slide4" className="carousel-item relative w-full flex justify-center align-middle">
-                        <img src={bill} className="h-40 rounded-full" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a> 
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
+                    {data.map((d)=> <Cor name={d.name} image={d.image} quote={d.quote} key={d.id} currSlide={d.currSlide}
+                      nextSlide={d.nextSlide} prevSlide={d.prevSlide}
+                      /> )}  
                     </div>
                      
                                          
